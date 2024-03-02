e852a96a6c248feae2e76f81c145bff6c97c655f
Your Name <you@example.com>
2024-03-02 14:55:58 +0300

    add bool on index.html

--- store/products/views.py
+++ store/products/views.py
@@ -4,7 +4,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        'title':'My App',
+        'is_promotion': False,
+    }
+    return render(request, 'products/index.html',context)
 
 
 def products(request):
